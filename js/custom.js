0031957f132ae4311dc88bc50f5add2128dd695c
anitatechnical97 <anitatechnical97@gmail.com>
2023-06-08 19:33:44 +0530

    JS variable naming convention

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+
+
+let a;                   // Declaration
+
+let b=10;                // Declaration and initialization
+
+//let b=20;              // Not allowed Redeclaration and initialization in let keyword
+
+
+var c=30;
+var c=40;           // Redeclaration and initialization allowed in var keyword
